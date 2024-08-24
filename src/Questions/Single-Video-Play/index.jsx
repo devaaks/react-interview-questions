834b6d359bca957ed2bb73cf6ba3c4e7b9a1c80f
Dev AKS <devaks24k@gmail.com>
2024-08-24 14:10:56 -0400

    feat: add single video play feature

--- src/Questions/Single-Video-Play/index.jsx
+++ src/Questions/Single-Video-Play/index.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Style.css';
 
 const SinglVideoPlay = () => {
-  const [currentVideo, setCurrentVideo] = useState(null);
+  const [currentVideoResolution, setCurrentVideoResolution] = useState(null);
+  const smallVideRef = useRef(null);
+  const largeVideRef = useRef(null);
 
-  const playVideo = () => {
-    // TBD
+  const playVideo = (resolution) => {
+  
+    if (currentVideoResolution) { // initial video play check 
+      // #1. if big video is playing then stop it
+      if (currentVideoResolution === '640x480') {
+        largeVideRef.current.pause();
+      }
+      // #2. if small video is playing then stop it
+      if (currentVideoResolution === '320x240') {
+        smallVideRef.current.pause();
+      }
+    }
+    setCurrentVideoResolution(resolution);
   }
 
   return (
     <div className='container'>
-      <video width="320" height="240" controls>
+      <video onPlay={() => playVideo('320x240')} width="320" height="240" controls ref={smallVideRef}>
         <source src="public/sample.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <video width="640" height="480" controls>
+      <video onPlay={() => playVideo('640x480')} width="640" height="480" controls ref={largeVideRef}>
         <source src="public/sample.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
