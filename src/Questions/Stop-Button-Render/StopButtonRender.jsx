c99138c0c6eafba1079b6161d9afeefcc859dd3c
Dev AKS <devaks24k@gmail.com>
2024-09-02 23:49:32 -0400

    chore: format

--- src/Questions/Stop-Button-Render/StopButtonRender.jsx
+++ src/Questions/Stop-Button-Render/StopButtonRender.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState } from 'react';
 
 function Msg({ count }) {
     return <h1>Count: {count}</h1>;
@@ -7,21 +7,20 @@ function Msg({ count }) {
 function Button({ increment }) {
     return <button onClick={increment}>Increment</button>;
 }
-  
+
 const StopButtonRender = () => {
     const [count, setCount] = useState(0);
-  
+
     const incrementCount = () => {
-      setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
     };
-  
+
     return (
-      <div>
-        <Msg count={count} />
-        <Button increment={incrementCount} />
-      </div>
+        <div>
+            <Msg count={count} />
+            <Button increment={incrementCount} />
+        </div>
     );
-  }
-  
-  export default StopButtonRender;
-  
+}
+
+export default StopButtonRender;
