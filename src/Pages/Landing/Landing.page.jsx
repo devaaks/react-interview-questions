2560a99c754d830d6d503c5ec0751cf29a57dcd0
Dev AKS <devaks24k@gmail.com>
2024-09-02 23:54:16 -0400

    feat: add navigation on main page

--- src/Pages/Landing/Landing.page.jsx
+++ src/Pages/Landing/Landing.page.jsx
@@ -5,6 +5,7 @@ const LandingPage = () => {
         <ol className="question-list">
             <li><a href='/random-color-generator'>Random Color Generator</a></li>
             <li><a href='/single-video-play'>Single Video Play</a></li>
+            <li><a href='/stop-button-render'>Stop Button Render</a></li>
         </ol>
     );
 }
