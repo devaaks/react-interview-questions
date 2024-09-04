b4a72475eae2a9b3162ff6919634f4c04edb2516
Dev AKS <devaks24k@gmail.com>
2024-09-03 23:57:25 -0400

    fix: import issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AppLayout from './layouts/app-layout';
 import LandingPage from './Pages/Landing/Landing.page';
 import ColorGenerator from './Questions/Random-Color-Generator/ColorGenerator';
 import SinglVideoPlay from './Questions/Single-Video-Play';
-import StopButtonRender from './Questions/Stop-Button-Render';
+import StopButtonRender from './Questions/Stop-Button-Render/StopButtonRender';
 
 const router = createBrowserRouter([
   {
